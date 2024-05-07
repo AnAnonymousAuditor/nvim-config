cd4051510063b9a24afab1eb6d4352993a6c3fe7
Manuel Botas <manuelbotas2001@gmail.com>
2024-05-07 03:06:13 -0300

    feat: enabled smart pairs for pear-tree

--- after/plugin/peartree.lua
+++ after/plugin/peartree.lua
@@ -1,1 +1,4 @@
 vim.cmd('let g:pear_tree_ft_disabled = ["TelescopePrompt"]')
+vim.cmd('let g:pear_tree_smart_openers = 1')
+vim.cmd('let g:pear_tree_smart_closers = 1')
+vim.cmd('let g:pear_tree_smart_backspace = 1')
