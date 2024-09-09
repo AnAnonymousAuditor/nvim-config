53f6d1a5d7f123e8782d21aca2856f3d1e0f3f72
ManuelBotas <manuelbotas2001@gmail.com>
2024-09-09 04:27:32 -0300

    add vimtex mappings

--- lua/manuelbotas/lazy/vimtex.lua
+++ lua/manuelbotas/lazy/vimtex.lua
@@ -4,14 +4,14 @@ return {
     init = function()
         -- VimTeX configuration goes here, e.g.
         vim.g.vimtex_view_method = "zathura_simple"
-        -- vim.g.vimtex_compiler_method = "tectonic"
-        -- vim.g.vimtex_compiler_tectonic = {
-        --     out_dir = "",
-        --     hooks = {},
-        --     options = {
-        --         "--keep-logs",
-        --         "--synctex",
-        --     },
-        -- }
+
+        -- Mappings
+        vim.keymap.set("n", "dsm", "<Plug>(vimtex-env-delete-math)")
+        vim.keymap.set("n", "csm", "<Plug>(vimtex-env-change-math)")
+        vim.keymap.set("n", "tsm", "<Plug>(vimtex-env-toggle-math)")
+        vim.keymap.set({ "x", "o" }, "ai", "<Plug>(vimtex-am)")
+        vim.keymap.set({ "x", "o" }, "ii", "<Plug>(vimtex-im)")
+        vim.keymap.set({ "x", "o" }, "am", "<Plug>(vimtex-a$)")
+        vim.keymap.set({ "x", "o" }, "im", "<Plug>(vimtex-i$)")
     end,
 }
