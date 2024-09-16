b40c2c5edbe64db731e8aa2739274626d061c3d7
ManuelBotas <manuelbotas2001@gmail.com>
2024-09-16 01:26:24 -0300

    remove undodir setting to use nvim default

--- lua/manuelbotas/set.lua
+++ lua/manuelbotas/set.lua
@@ -16,7 +16,7 @@ vim.opt.showmode = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+--vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.splitright = true
